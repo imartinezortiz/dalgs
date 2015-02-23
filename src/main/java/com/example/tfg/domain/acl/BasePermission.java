7ffea052d722eb293ac25b78bd5be13b3b0f4ea2
Almudena Fernandez Romo <romo.almudenafer@gmail.com>
2015-02-23 23:18:07 +0100

    Añadidas las tablas acl. acl-context.xml modificado pom

---
+++ src/main/java/com/example/tfg/domain/acl/BasePermission.java
@@ -0,0 +1,25 @@
+package com.example.tfg.domain.acl;
+
+/*Default  permissions to use in the ACL system*/
+import org.springframework.security.acls.domain.AbstractPermission;
+import org.springframework.security.acls.model.Permission;
+
+public class BasePermission extends AbstractPermission {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public static final Permission READ = new BasePermission(1 << 0, 'R'); // 1
+	public static final Permission WRITE = new BasePermission(1 << 1, 'W'); // 2
+	public static final Permission CREATE = new BasePermission(1 << 2, 'C'); // 4
+	public static final Permission DELETE = new BasePermission(1 << 3, 'D'); // 8
+	public static final Permission ADMINISTRATION = new BasePermission(1 << 4, 'A'); // 16
+
+	protected BasePermission(int mask) {
+		super(mask);
+	}
+
+	protected BasePermission(int mask, char code) {
+		super(mask, code);
+	}
+}
