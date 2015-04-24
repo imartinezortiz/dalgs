14909f058929d3115392e144a3c690273226b4de
Almudena Fernandez Romo <romo.almudenafer@gmail.com>
2015-04-24 18:33:27 +0200

    curl -v -X POST    -H "Content-Type: apon/json"  -H "Authorization: Basic YWRtaW46YWRtaW4=" 'http://localhost:8081/dalgs/oauth/token?grant_type=password&username=admin&password=admin'

--- src/main/java/es/ucm/fdi/dalgs/rest/web/WebhookUriConstants.java
+++ src/main/java/es/ucm/fdi/dalgs/rest/web/WebhookUriConstants.java
@@ -3,11 +3,11 @@ package es.ucm.fdi.dalgs.rest.web;
 public class WebhookUriConstants {
 
 	 
-	    public static final String POST_ACTIVITY = "/rest/activity";
+	    public static final String POST_ACTIVITY = "/api/activity";
 
-	    public static final String GET_ACTIVITY = "/rest/activity/{id}";
+	    public static final String GET_ACTIVITY = "/api/activity/{id}";
 	    
-	    public static final String AUTH_DENIED = "/rest/denied";
+	    public static final String AUTH_DENIED = "/api/denied";
 	
 }
 
