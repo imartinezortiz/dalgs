005ec5c51cc7b508f735681e1ea05f0319a91c86
Almudena Fernandez Romo <romo.almudenafer@gmail.com>
2015-04-28 13:03:08 +0200

    License applied

--- src/main/webapp/resources/scripts/bootstrap-filestyle.js
+++ src/main/webapp/resources/scripts/bootstrap-filestyle.js
@@ -1,11 +1,18 @@
 /*
- * bootstrap-filestyle
- * doc: http://markusslima.github.io/bootstrap-filestyle/
- * github: https://github.com/markusslima/bootstrap-filestyle
+ * This file is part of D.A.L.G.S.
  *
- * Copyright (c) 2014 Markus Vinicius da Silva Lima
- * Version 1.1.2
- * Licensed under the MIT license.
+ * D.A.L.G.S is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * D.A.L.G.S is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with D.A.L.G.S.  If not, see <http://www.gnu.org/licenses/>.
  */
 (function($) {"use strict";
 
